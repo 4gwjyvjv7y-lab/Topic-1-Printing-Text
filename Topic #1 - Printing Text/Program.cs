428c9da95b66cf082efdb7a6036ea3f7379c9142
339821241 <339821241@2260I-L29-033>
2026-02-03 13:06:33 -0500

    Add project files.

---
+++ Topic #1 - Printing Text/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Topic__1___Printing_Text
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("My Name Is Owen Austin");
+            Console.WriteLine("What is your name?");
+            Console.WriteLine("");
+            string? a = Console.ReadLine();
+            Console.WriteLine("Hello " + a + ", nice to meet you!");
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("what is your favorite food " + a + "?");
+            Console.WriteLine("");
+            string? b = Console.ReadLine();
+            Console.WriteLine("You like " + b + "?");
+            Console.WriteLine("Sounds Great!");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+            Console.Clear();
+
+
+
+        }
+    }
+}
