22a6bc387dffa1e405ab8f96214c1d19b6344b16
339821241 <339821241@2260I-L29-033>
2026-02-04 12:32:53 -0500

    about to start assignment

--- Topic #1 - Printing Text/Program.cs
+++ Topic #1 - Printing Text/Program.cs
@@ -4,24 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("My Name Is Owen Austin");
-            Console.WriteLine("What is your name?");
-            Console.WriteLine("");
-            string? a = Console.ReadLine();
-            Console.WriteLine("Hello " + a + ", nice to meet you!");
-            Console.WriteLine("");
-            Console.WriteLine("Press any key to continue");
-            Console.ReadLine();
-            Console.Clear();
-            Console.WriteLine("what is your favorite food " + a + "?");
-            Console.WriteLine("");
-            string? b = Console.ReadLine();
-            Console.WriteLine("You like " + b + "?");
-            Console.WriteLine("Sounds Great!");
-            Console.WriteLine("Press any key to continue");
-            Console.ReadLine();
-            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkBlue ;
+            c
 
 
 
