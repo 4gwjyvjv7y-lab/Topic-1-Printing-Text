676c591392b0ba4824e810219a9183a2be1289d3
339821241 <339821241@2260I-L29-033>
2026-02-04 13:17:16 -0500

    Done assignment

--- Topic #1 - Printing Text/Program.cs
+++ Topic #1 - Printing Text/Program.cs
@@ -1,11 +1,30 @@
-﻿namespace Topic__1___Printing_Text
+﻿namespace Topic__1___Printing_Text//Owen Austin
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.DarkBlue ;
-            c
+            Console.WriteLine("Hello My Name Is Owen Austin");
+            Console.WriteLine("");
+            Console.WriteLine("Comments are very important in programming and are used to");
+            Console.WriteLine("Help yourself remember things and give information to other Programmers");
+            Console.WriteLine();
+            Console.WriteLine(" OOO   ");
+            Console.WriteLine("O   O  ");
+            Console.WriteLine("O   O  ");
+            Console.WriteLine("O   O  ");
+            Console.WriteLine(" OOO   ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("  AA  ");
+            Console.WriteLine(" A  A ");
+            Console.WriteLine("AAAAAA");
+            Console.WriteLine("A    A");
+            Console.WriteLine("A    A");
+            //This is a simple representation of my initials OA
+
 
 
 
